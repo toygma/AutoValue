9ccce0e4b1bc9b13c22542e01edf8cc7dc63c4a9
toygma <toygunbektasoglu@gmail.com>
2025-11-04 21:40:04 +0300

    fixed: local build completed

--- app/cars/[id]/_components/Reviews.tsx
+++ app/cars/[id]/_components/Reviews.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { MessageCircle, X } from "lucide-react";
+import { MessageCircle } from "lucide-react";
 import { toast } from "react-hot-toast";
 import axiosInstance from "@/lib/axios";
 import { useSession } from "next-auth/react";
