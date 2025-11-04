796e0c4d55baf08bcf8c36681d2e9ae11d2baa01
toygma <toygunbektasoglu@gmail.com>
2025-11-04 23:23:56 +0300

    fixed:prisma generate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,9 @@
-import { withAccelerate } from '@prisma/extension-accelerate'
 import { PrismaClient } from './generated/prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
-}
+const prisma = new PrismaClient().$extends(withAccelerate())
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+const globalForPrisma = global as unknown as { prisma: typeof prisma }
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
